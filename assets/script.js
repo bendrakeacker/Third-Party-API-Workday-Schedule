9bc4e09b07e179db8b159e716395038bab3bfd06
Ben Acker <bda5464@nyu.edu>
2020-06-14 15:03:28 -0400

    outlined js file

--- assets/script.js
+++ assets/script.js
@@ -4,5 +4,29 @@ var inTime = "";
 var schedArr = JSON.parse(localStorage.getItem("Schedule")) || [];
 
 function clock() {
-    var time = setInterval(function()
+    
 }
+
+function present() {
+
+}
+
+function past() {
+
+}
+
+function future() {
+
+}
+
+$(".saveBtn").on("click",function(){
+
+});
+
+function schedule() {
+
+}
+
+$(".resetBtn").on("click",function(){
+
+});
