4bbeafa9b45dfc7c61711ca7155bc57234067813
Ben Acker <bda5464@nyu.edu>
2020-06-15 16:50:09 -0400

    finalized script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Third-Party-API-Workday-Schedule
+The workday scheduler is a jquery-powered, moment js supplemented, and bootstrap-gridded planner for daily scheduling. It displays the hours of a standard workday, and displays different colors to display hours that have past, the current hour, and future hours (red, green, and blue, respectively). In addition, when a scheduling event is added to an hour block, it is saved using local storage so the schedule can be referred to throughout the day. 
+
+In the script you will find three functions. Firstly, a function to set the time to local time and create a for loop to make 8 separate timeblocks, each corresponding to an id that is equal to the military time of the timeblock. This function can then separate the past present and future timeblocks by checking if the id matches to the hour it is now.
+
+The second function is simply a click function to save text to local storage, and the third function displays it back to the timeblock it was entered in so refreshing the page holds the scheduled event.
+
+![](assets/schedule.png)
--- assets/script.js
+++ assets/script.js
@@ -7,71 +7,56 @@ var schedArr = JSON.parse(localStorage.getItem("Schedule")) || [];
 
 //function tracks current time and updates site display accordingly
 function clock() {
-    timer = setInterval(function(){
-        hrNow = moment().format('hA');
+        hrNow = moment().format('h');
         inTime = moment().format('dddd, MMMM Do YYYY');
         $("#today").text(inTime);
-        determineTime();
+        
+        var id = "09";
+        var hrMil = moment().format('HH');
+        for(var i = 9; i <= 17; i++ ) {
+            var rowDiv = $("<div id=slot-"+i+">").addClass("row hour");
+            var hrTxt = moment().hour(i).format('HH');
+            var hrCol = $("<section>").addClass("col col-lg col-md col-sm").text(hrTxt);
+            var txt = $("<textarea>").addClass("col-8 col-lg-8 col-md-8 col-sm-8 textarea");
+            var btn = $("<button>").addClass("col col-lg col-md col-sm saveBtn").text("SAVE");
+            
+            hrCol.attr("id", id);
+            rowDiv.append(hrCol,txt,btn);
+            $(".container").append(rowDiv);
+            // console.log(id);
+            id++
+           
 
-    })
+            if((hrCol.attr("id")) == hrMil) {
+                hrCol.addClass("now");
+            } 
+            else if(parseInt(hrCol.attr("id")) > hrMil) {
+                hrCol.addClass("before");
+            }
+            else if(parseInt(hrCol.attr("id")) < hrMil) {
+                hrCol.addClass("after");
+            }
+            saveSchedule();
+            
+        }
 }
 clock();
 
-function determineTime() {
-    $(".hour").each(function() {
-
-        var hrNowBlock = hrNow;
-        var hrBlock = $(this).children("section").html();
-        var blockAMPM = "";
-        var hrNowBlockAMPM = "";
-//if the hour has a string of 4 (for blockAMPM)
-        if(hrBlock.length == 4) {
-            blockAMPM = hrBlock[2]+hrNowBlockAMPM[3];
-            hrNowBlock = hrNowBlock[1];
-        }
-//if the string doesnt have 4 characters it must 5
-        else {
-            blockAMPM = hrBlock[3] + hrNowBlockAMPM[4];
-            hrNowBlock = hrNowBlock[1] + hrNowBlock[2];
-        }
-// same conditional for hrNowBlockAMPM
-        if(hrNowBlock.length == 4) {
-            hrNowBlockAMPM = hrNowBlock[2] + hrNowBlock[3];
-            hrNowBlock = hrNowBlock[1];
-        }
-        else {
-            hrNowBlockAMPM = hrNowBlock[3] + hrNowBlock[4];
-            hrNowBlock = hrNowBlock[1] + hrNowBlock[2];
-        }
-
-        hrNowBlock = parseInt(hrNowBlock);
-        hrBlock = parseInt(hrBlock);
-
-        if($(this).children("section").html() == (hrNowBlock + hrBlock)) {
-            $(this).children("textarea").addClass("now");
-        }
-
-        if(hrBlock < hrNowBlock && hrBlock !== 12 && blockAMPM == hrNowBlockAMPM) {
-            $(this).children("textarea").addClass("before");
-        }
-
-        else if(hrBlock == 12 && hrNowBlock >= 1 && hrNowBlockAMPM == "PM" && hrNow !== 12){
-            $(this).children("textarea").addClass("before");
-        }
-        else if(hrBlock == 12 && hrNowBlock >=1 && blockAMPM == "AM" && hrNowBlockAMPM == "AM") {
-            $(this).children("textarea").addClass("before");
-        }
-        if(blockAMPM == "AM" && hrNowBlockAMPM == "PM"){
-            $(this).children("textarea").addClass("before");
-        }
-    })
-}
-
-
 $(".saveBtn").on("click",function(){
-
+    var txtArea = $(this).parent().children("textarea").val();
+    var currentHr = $(this).parent().children("section").text();
+    schedArr.push({msg: txtArea, hour: currentHr});
+    localStorage.setItem("Schedule", JSON.stringify(schedArr));
 });
 
-function schedule() {
+function saveSchedule() {
+    for(var i=0; i < schedArr.length; i++){
+        var msg =schedArr[i].msg;
+        var hour = schedArr[i].hour;
 
+        $(".hour").each(function(){
+            if($(this).children("section").text() === hour)
+                $(this).children("textarea").text(msg);
+        });
+    }
 }
