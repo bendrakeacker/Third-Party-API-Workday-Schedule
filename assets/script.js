363d5ac02042ae84d8877e516c9f020d1c09527a
Ben Acker <bda5464@nyu.edu>
2020-06-15 08:27:03 -0400

    variables converted to integers and console logged; ready for present/past/future distinguishing.

--- assets/script.js
+++ assets/script.js
@@ -16,10 +16,39 @@ function clock() {
 
     })
 }
+clock();
 
 function determineTime() {
-    
+    $(".hour").each(function() {
 
+        var hrNowBlock = hrNow;
+        var hrBlock = $(this).children("section").html();
+        var blockAMPM = "";
+        var hrNowBlockAMPM = "";
+
+        if(hrBlock.length == 4) {
+            blockAMPM = hrBlock[2]+hrNowBlockAMPM[3];
+            hrNowBlock = hrNowBlock[1];
+        }
+        else {
+            blockAMPM = hrBlock[4] + hrNowBlockAMPM[5];
+            hrNowBlock = hrNowBlock[0] + hrNowBlock[1];
+        }
+        if(hrNowBlock.length == 4) {
+            hrNowBlockAMPM = hrNowBlock[2] + hrNowBlock[3];
+            hrNowBlock = hrNowBlock[1];
+        }
+        else {
+            hrNowBlockAMPM = hrNowBlock[3] + hrNowBlock[4];
+            hrNowBlock = hrNowBlock[1] + hrNowBlock[2];
+        }
+
+        hrNowBlock = parseInt(hrNowBlock);
+        hrBlock = parseInt(hrBlock);
+
+        console.log(hrNowBlock);
+        console.log(hrBlock);
+    })
 }
 
 
