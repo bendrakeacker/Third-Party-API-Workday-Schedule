a3960c9749982fba9c783592e70fd10f96078d9a
Ben Acker <bda5464@nyu.edu>
2020-06-15 08:09:50 -0400

    updated functions

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,27 @@
+//necessary time variables
 var hrNow = "";
 var inTime = "";
 
+//set an array object to store scheduling events 
 var schedArr = JSON.parse(localStorage.getItem("Schedule")) || [];
 
+//function tracks current time and updates site display accordingly
 function clock() {
+    timer = setInterval(function(){
+        hrNow = moment().format('hA');
+        inTime = moment().format('dddd, MMMM Do YYYY');
+        $("#today").text(inTime);
+        determineTime();
+
+
+    })
+}
+
+function determineTime() {
     
-}
-
-function present() {
 
 }
 
-function past() {
-
-}
-
-function future() {
-
-}
 
 $(".saveBtn").on("click",function(){
 
